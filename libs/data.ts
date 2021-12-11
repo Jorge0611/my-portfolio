9147e825d10f58447a1efdcc5a4e51fdc833b364
Jorge0611 <49691994+Jorge0611@users.noreply.github.com>
2021-12-10 21:58:52 -0400

    Project upload

---
+++ libs/data.ts
@@ -0,0 +1,56 @@
+export const data = [
+  {
+    id: "windpress",
+    name: "Windpress",
+    description:
+      "a website created to manage your tasks, create, edit, or delete them, while also setting a due time to do each task. This app was created with nextjs, tailwind and mongodb.",
+    preview: "/windpres.png",
+    repoUrl: "https://github.com/Jorge0611/windpress-app",
+    hostUrl: "",
+  },
+  {
+    id: "firebase-crud",
+    name: "Firebase CRUD",
+    description:
+      "a minimal crud application to manage save notes, edit or delete them. This app was created using create-react-app, firebase, firestore and tailwind css.",
+    preview: "/firebase-crud-app.png",
+    repoUrl: "https://github.com/Jorge0611/firebase-crud",
+    hostUrl: "",
+  },
+  {
+    id: "github-api-app",
+    name: "GitHub Api App",
+    description:
+      "a minimal app to see github repositories, and also filter them by language. created using the github api and octokit to make the calls to the api.",
+    preview: "/github-api-app.png",
+    repoUrl: "https://github.com/Jorge0611/api-github-app",
+    hostUrl: "https://api-github-app.vercel.app/",
+  },
+  {
+    id: "express-api",
+    name: "express-api",
+    description:
+      "a backend with routes token authentication, depending on roles and user level. This api was created using expressjs, mongodb, mongoose, and jsonwebtoken.",
+    preview: "/express-api.png",
+    repoUrl: "https://github.com/Jorge0611/express-api",
+    hostUrl: "",
+  },
+  {
+    id: "instagram-clone",
+    name: "Instagram Clone",
+    description:
+      "a clone of the instagram homepage user interface. created with html css and tailwind css as cdn.",
+    preview: "/instagram-clone.png",
+    repoUrl: "https://github.com/Jorge0611/instagram-clone",
+    hostUrl: "",
+  },
+  {
+    id: "hrrecognition",
+    name: "HrRecognition",
+    description:
+      "a mobile application built to execute specific tasks depending on your hand gestures, and also, detect faces previously saved. This app was created using android, firebase ML Kit, and tensorflow lite for face recognition.",
+    preview: "/hrrecognition.jpg",
+    repoUrl: "https://github.com/Jorge0611/android",
+    hostUrl: "",
+  },
+];
